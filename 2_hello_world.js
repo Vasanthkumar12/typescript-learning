c5baa8e6e1d3d543d2dda5ce55137cdba8e89c22
Vasanthkumar M <vasanth23402@gmail.com>
2025-05-08 10:47:43 +0530

    Hello world program done

---
+++ 2_hello_world.js
@@ -0,0 +1,5 @@
+// We can write Javascript code also inside Typescript file because TS is a superset of Javascript. But we cannot write ts code inside js file.
+function greet() {
+    console.log("Hello World, remeber Jesus created you for the purpose of live with him.");
+}
+greet();
